daa2d79d00d901f6fbdd71dc820f05351e47c5eb
ADE-KHOERUDIN <adekhoerudin097@gmail.com>
2020-09-05 00:14:32 +0700

    update Tugas 9

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
 import React from 'react';
-import './App.css';
+import FormPembelian from  './Tugas-9/FormPembelian';
+import HargaBuah from './Tugas-10/HargaBuah';
 
 function App() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', borderRadius: '10px',  border: '1px solid', width: '50%', margin: '10% 25%',  }}>
-      <div style={{ width: '50%', }}>
-        <h2 style={{ textAlign: 'center', }}>Form Pembelian Buah</h2>
-        <table style={{ border: 0, width: '100%', }}>
-          <tbody>
-            <tr>
-              <td style={{ fontWeight: 'bold', }}>Nama Pelanggan</td>
-              <td><input type="text" /></td>
-            </tr>
-            <tr>
-              <td style={{ fontWeight: 'bold', position: 'absolute',}}>Daftar Items</td>
-              <td>
-                <input type="checkbox" />
-                Semangka 
-                <br />
-                <input type="checkbox" />
-                Jeruk 
-                <br />
-                <input type="checkbox" />
-                Nanas 
-                <br />
-                <input type="checkbox" />
-                Salak 
-                <br />
-                <input type="checkbox" />
-                Anggur 
-                <br />
-              </td>
-            </tr>
-            <tr>
-              <td colSpan="2">
-                <button type="button" style={{ height: '29px', borderRadius: '10px',}}>Kirim</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+    <div>
+      <FormPembelian />
+      <HargaBuah />
     </div>
-    );
-  }
-  
-  export default App;
+  );
+}
+
+export default App;
