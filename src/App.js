f1915813ff1710fe1dd2fae462d27e3805964f2f
ADE-KHOERUDIN <adekhoerudin097@gmail.com>
2020-09-07 22:54:27 +0700

    Update Tugas-11

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import FormPembelian from  './Tugas-9/FormPembelian';
 import HargaBuah from './Tugas-10/HargaBuah';
+import Timer from './Tugas-11/Timer';
 
 function App() {
   return (
     <div>
       <FormPembelian />
       <HargaBuah />
+      <Timer countdown='101'/>
     </div>
   );
 }
