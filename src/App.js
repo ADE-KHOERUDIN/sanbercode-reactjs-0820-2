a54ad5ee28d54ccb184778684fc0ada37ac73e88
ADE-KHOERUDIN <adekhoerudin097@gmail.com>
2020-09-09 08:07:18 +0700

    update Tugas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import FormPembelian from  './Tugas-9/FormPembelian';
 import HargaBuah from './Tugas-10/HargaBuah';
 import Timer from './Tugas-11/Timer';
+import ListHargaBuah from './Tugas-12/ListHargaBuah';
 
 function App() {
   return (
